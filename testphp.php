7e0cf0271b3a6c5c566b3462a81fc3740fa18a68
GulliverJ <gully.johnson@gmail.com>
2015-03-11 23:07:13 +0000

    Testing php 9

---
+++ testphp.php
@@ -0,0 +1,63 @@
+<html>
+<body>
+
+<?php
+    // DB connection info
+    $host = "eu-cdbr-azure-north-c.cloudapp.net";
+    $user = "b9b9fc737b7f9b";
+    $pwd = "8bce3b67";
+    $db = "isdevAnAqTBTyio8";
+    // Connect to database.
+    try {
+        $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    }
+    catch(Exception $e){
+        die(var_dump($e));
+    }
+    // Insert registration info
+
+    /*
+    if(!empty($_POST)) {
+    try {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $date = date("Y-m-d");
+        // Insert data
+        $sql_insert = "INSERT INTO registration_tbl (name, email, date) 
+                   VALUES (?,?,?)";
+        $stmt = $conn->prepare($sql_insert);
+        $stmt->bindValue(1, $name);
+        $stmt->bindValue(2, $email);
+        $stmt->bindValue(3, $date);
+        $stmt->execute();
+    }
+    catch(Exception $e) {
+        die(var_dump($e));
+    }
+    echo "<h3>Your're registered!</h3>";
+    }
+    */
+
+    // Retrieve data
+    $sql_select = "SELECT * FROM testuser";
+    $stmt = $conn->query($sql_select);
+    $registrants = $stmt->fetchAll(); 
+    if(count($registrants) > 0) {
+        echo "<h2>People who are registered:</h2>";
+        echo "<table>";
+        echo "<tr><th>id</th>";
+        echo "<th>fname</th>";
+        echo "<th>lname</th></tr>";
+        foreach($registrants as $registrant) {
+            echo "<tr><td>".$registrant['id']."</td>";
+            echo "<td>".$registrant['firstName']."</td>";
+            echo "<td>".$registrant['lastName']."</td></tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "<h3>No one is currently registered.</h3>";
+    }
+?>
+</body>
+</html>
