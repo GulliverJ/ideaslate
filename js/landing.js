4916955796b1fbff7e432b78be12c5a83969de34
GulliverJ <gully.johnson@gmail.com>
2015-03-18 14:15:00 +0000

    Added initial User Home UI mockup, with mostly empty pages

--- js/landing.js
+++ js/landing.js
@@ -1,10 +1,23 @@
+var curView = "dashboard";
+
 function autoScrollTo(el) {
     var top = $("#" + el).offset().top;
     $("html, body").animate({ scrollTop: top }, 400);
 }
 
-$(document).ready(function() {
-    $('#signup').click(function() {
-        $('.signup').slideToggle(300);
+function switchTo(panel) {
+    $(document).ready(function() {
+
+        if (curView == panel) {
+          if (curView == "dashboard") {
+            return;
+          }
+          panel = "dashboard";
+        }
+
+        $("." + panel).slideToggle(300);
+        $("." + curView).slideToggle(300);
+        curView = panel;
+        
     });
-});
+}
